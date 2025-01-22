95927e47fd3d34bfe404ad40b0d83cf8e8013390
possxx <168581964+possxx@users.noreply.github.com>
2025-01-22 19:48:09 +0100

    Setup babel and jest

--- webpack.common.js
+++ webpack.common.js
@@ -14,6 +14,13 @@ module.exports = {
 	module: {
 		rules: [
 			{
+				test: /\.js$/i,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+			{
 				test: /\.css$/i,
 				use: [MiniCSSExtractPlugin.loader, 'css-loader'],
 				exclude: /\.module\.css$/i,
