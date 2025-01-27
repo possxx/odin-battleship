2e743f1582a14145cef96ab2f7a4b13421f89d4a
possxx <168581964+possxx@users.noreply.github.com>
2025-01-27 17:46:44 +0100

    Add Player classes

---
+++ src/computer.js
@@ -0,0 +1,7 @@
+import Player from './player';
+
+export default class Computer extends Player {
+	constructor() {
+		super();
+	}
+}
---
+++ src/player.js
@@ -0,0 +1,7 @@
+import Gameboard from './gameboard';
+
+export default class Player {
+	constructor() {
+		this.gameboard = new Gameboard();
+	}
+}
