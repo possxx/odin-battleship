3c364eb5634f43ef4efa0f7fa9716a497b0077ef
possxx <168581964+possxx@users.noreply.github.com>
2025-01-31 20:22:13 +0100

    Create game instance and initiate game

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import Controller from './dom/controlsDOM';
+
+const pos1 = [
+	[0, 0],
+	[0, 1],
+	[0, 2],
+];
+const pos2 = [
+	[9, 9],
+	[9, 8],
+	[9, 7],
+	[9, 6],
+	[9, 5],
+];
+
+const controller = new Controller();
+controller.initGame();
