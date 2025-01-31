7d36085db8a731a2910a1302648193c57bf4a718
possxx <168581964+possxx@users.noreply.github.com>
2025-01-31 20:19:35 +0100

    Keep track of moves and only allow unique moves

--- src/computer.js
+++ src/computer.js
@@ -3,9 +3,21 @@ import Player from './player';
 export default class Computer extends Player {
 	constructor() {
 		super();
+		this.attacks = new Set();
 	}
 
 	getMove() {
-		return [Math.floor(Math.random() * 10), Math.floor(Math.random() * 10)];
+		let move;
+
+		do {
+			move = [
+				Math.floor(Math.random() * 10),
+				Math.floor(Math.random() * 10),
+			];
+		} while (this.attacks.has(move.toString()));
+
+		this.attacks.add(move.toString());
+
+		return move;
 	}
 }
