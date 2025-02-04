4b8a15eadbea2359a129dce4babdeb09a46c0a0d
possxx <168581964+possxx@users.noreply.github.com>
2025-02-04 20:01:17 +0100

    Add computer move methods to make intelligent moves and adjust tests

--- src/computer.js
+++ src/computer.js
@@ -1,12 +1,22 @@
 import Player from './player';
 
 export default class Computer extends Player {
-	constructor() {
+	constructor(playerBoard) {
 		super();
 		this.attacks = new Set();
+		this.playerBoard = playerBoard;
+		this.attackDirection = null;
+		this.currentShipAttacks = null;
 	}
 
 	getMove() {
+		if (this.currentShipAttacks) {
+		}
+	}
+
+	determineMove(shipAttacks) {}
+
+	getRandomMove() {
 		let move;
 
 		do {
@@ -17,6 +27,51 @@ export default class Computer extends Player {
 		} while (this.attacks.has(move.toString()));
 
 		this.attacks.add(move.toString());
+
+		return move;
+	}
+
+	getAdjacentMove(hit) {
+		let move;
+		let possibleMoves = [
+			[0, 1],
+			[0, -1],
+			[1, 0],
+			[-1, 0],
+		];
+
+		for (let i = 0; i <= 3; i++) {
+			if (
+				move &&
+				!this.attacks.has(move.toString()) &&
+				move[0] >= 0 &&
+				move[0] <= 9 &&
+				move[1] >= 0 &&
+				move[1] <= 9
+			)
+				break;
+
+			move = [hit[0] + possibleMoves[i][0], hit[1] + possibleMoves[i][1]];
+		}
+
+		move[0] === hit[0]
+			? (this.attackDirection = 'vertical')
+			: (this.attackDirection = 'horizontal');
+
+		return move;
+	}
+
+	getNextMove(lastHit, direction, sign) {
+		let move;
+
+		if (direction === 'vertical')
+			sign === '-'
+				? (move = [lastHit[0], lastHit[1] - 1])
+				: (move = [lastHit[0], lastHit[1] + 1]);
+		if (direction === 'horizontal')
+			sign === '-'
+				? (move = [lastHit[0] - 1, lastHit[1]])
+				: (move = [lastHit[0] + 1, lastHit[1]]);
 
 		return move;
 	}
--- src/tests/computer.test.js
+++ src/tests/computer.test.js
@@ -3,8 +3,46 @@ import Computer from '../computer';
 describe('computer works as expected', () => {
 	let computer = new Computer();
 
+	test('returns next move after successful hit on adjacent cell', () => {
+		let lastHit = [5, 5];
+		let nextMove = computer.getNextMove(lastHit, 'horizontal', '-');
+		expect(nextMove).toEqual([4, 5]);
+
+		let lastHit2 = [7, 7];
+		let nextMove2 = computer.getNextMove(lastHit2, 'vertical');
+		expect(nextMove2).toEqual([7, 8]);
+	});
+
+	test('returns adjacent moves', () => {
+		let move = [0, 0];
+		let possibleMoves = [
+			[0, 1],
+			[1, 0],
+		];
+
+		let adjacentMove = computer.getAdjacentMove(move);
+
+		expect(possibleMoves).toContainEqual(
+			expect.arrayContaining(adjacentMove)
+		);
+
+		let move2 = [5, 5];
+		let possibleMoves2 = [
+			[5, 6],
+			[5, 4],
+			[6, 5],
+			[6, 4],
+		];
+
+		let adjacentMove2 = computer.getAdjacentMove(move2);
+
+		expect(possibleMoves2).toContainEqual(
+			expect.arrayContaining(adjacentMove2)
+		);
+	});
+
 	test('returns array with random positive integer x, y values below 10', () => {
-		let move = computer.getMove();
+		let move = computer.getRandomMove();
 		expect(move[0]).toBeGreaterThanOrEqual(0);
 		expect(move[0]).toBeLessThan(10);
 		expect(move[1]).toBeGreaterThanOrEqual(0);
